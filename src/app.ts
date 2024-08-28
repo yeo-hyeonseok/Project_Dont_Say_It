8f691a52e85edf63196e81a8077fedc2e2c192e6
dugustlr@naver.com <dugustlr@naver.com>
2024-08-28 14:00:46 +0900

    [ADD] 라우터 적용 & views 파일 레이아웃 분리

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import mainRouter from "./routes/main";
 
 const app = express();
 
@@ -9,8 +10,6 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 
 /* Router */
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use("/", mainRouter);
 
 app.listen(3000, () => console.log("3000번 포트 연결 중..."));
