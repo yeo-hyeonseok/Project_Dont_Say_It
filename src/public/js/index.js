6ce79080688d12a7f24cfb88f76a0bd8c20950fc
dugustlr@naver.com <dugustlr@naver.com>
2024-09-14 23:26:03 +0900

    [ADD] 대화방 헤더 타이머 기능 추가
    - 기본 시간은 2분으로 설정

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,6 +1,30 @@
 const socket = io();
 
-/* room */
+/* ---- room ---- */
+/* 나가기 버튼 */
 const exitButton = document.querySelector("span.exit_button");
 
 exitButton.addEventListener("click", () => history.back());
+
+/* 타이머 */
+const timer = document.querySelector("span.timer");
+
+let time = 120;
+let timeInterval;
+
+function startTimer() {
+  timeInterval = setInterval(() => {
+    if (time > 0) {
+      time--;
+
+      const minutes = Math.floor(time / 60);
+      const seconds = time % 60;
+
+      timer.innerText = `${minutes}:${seconds > 9 ? seconds : `0${seconds}`}`;
+    } else {
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+}
+
+startTimer();
