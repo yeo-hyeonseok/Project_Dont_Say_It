1a3779f78b695bce44c15c7d9afab34e3f6ffb3d
dugustlr@naver.com <dugustlr@naver.com>
2024-10-14 12:59:13 +0900

    [UPDATE] app.ts 파일 간소화를 위해서 소켓 관련 로직 다른 파일로 분리
    - 'src/socket.ts' 파일로 분리함

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,10 @@ import path from "path";
 import compression from "compression";
 import helmet from "helmet";
 import http from "http";
-import { Server, Socket } from "socket.io";
 import cookieParser from "cookie-parser";
-import shortid from "shortid";
 import mainRouter from "./routes/main";
 import roomRouter from "./routes/room";
-import { getRandomWord } from "./data/words";
-import { getRandomTopic } from "./data/topics";
+import setWebSocket from "./socket";
 
 const app = express();
 const defaultDirectives = helmet.contentSecurityPolicy.getDefaultDirectives();
@@ -44,96 +41,7 @@ app.use("/room", roomRouter);
 
 /* Socket */
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer);
 
-function getRandomIndex(length: number) {
-  return Math.floor(Math.random() * length);
-}
-
-function getPublicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = wsServer;
-
-  return Array.from(rooms).filter((room) => !sids.get(room[0]));
-}
-
-wsServer.on("connection", (socket: Socket) => {
-  console.log("소켓 연결됨:", socket.id);
-
-  let roomName: string;
-  let time = 120;
-  let timeInterval: NodeJS.Timeout | undefined;
-  const myWord: string = getRandomWord();
-  const otherWord: string = getRandomWord();
-
-  socket.on("user_match", () => {
-    const filtered = Array.from(getPublicRooms()).filter(
-      (room) => room[1].size < 2
-    );
-
-    if (filtered.length > 0) {
-      // 빈 방 있으면 참여
-      roomName = filtered[getRandomIndex(filtered.length)][0];
-
-      socket.join(roomName);
-      wsServer.to(roomName).emit("send_welcome", roomName, getRandomTopic());
-      socket.emit("send_myword", myWord);
-      socket.to(roomName).emit("send_otherword", otherWord);
-    } else {
-      // 빈 방 없으면 방 생성
-      roomName = shortid.generate();
-
-      socket.join(roomName);
-    }
-  });
-
-  socket.on("start_timer", () => {
-    timeInterval = setInterval(() => {
-      if (time > 0) {
-        time--;
-
-        socket.emit("time_change", time);
-      } else {
-        clearInterval(timeInterval);
-      }
-    }, 1000);
-  });
-
-  socket.on("adjust_time", (amount: number, done: () => void) => {
-    if (time === 120) return;
-
-    if (amount > 0 ? time <= 100 : time >= 20) {
-      time += amount;
-
-      wsServer.to(roomName).emit("adjust_time", time);
-      wsServer
-        .to(roomName)
-        .emit(
-          "send_notice",
-          socket.id,
-          amount > 0 ? "시간을 연장했습니다." : "시간을 단축했습니다."
-        );
-      done();
-    }
-  });
-
-  socket.on("sync_time", (currentTime) => {
-    time = currentTime;
-  });
-
-  socket.on("send_message", (msg: string, done: () => void) => {
-    if (time === 120) return;
-
-    socket.to(roomName).emit("send_message", msg);
-    done();
-  });
-
-  socket.on("disconnect", () => {
-    console.log("소켓 연결 해제됨");
-  });
-});
+setWebSocket(httpServer);
 
 httpServer.listen(3000, () => console.log("3000번 포트 연결 중..."));
---
+++ src/socket.ts
@@ -0,0 +1,99 @@
+import http from "http";
+import shortid from "shortid";
+import { Server, Socket } from "socket.io";
+import { getRandomWord } from "./data/words";
+import { getRandomTopic } from "./data/topics";
+
+function setWebSocket(server: http.Server) {
+  const io = new Server(server);
+
+  function getRandomIndex(length: number) {
+    return Math.floor(Math.random() * length);
+  }
+
+  function getPublicRooms() {
+    const {
+      sockets: {
+        adapter: { sids, rooms },
+      },
+    } = io;
+
+    return Array.from(rooms).filter((room) => !sids.get(room[0]));
+  }
+
+  io.on("connection", (socket: Socket) => {
+    console.log("소켓 연결됨:", socket.id);
+
+    let roomName: string;
+    let time = 120;
+    let timeInterval: NodeJS.Timeout | undefined;
+    const myWord: string = getRandomWord();
+    const otherWord: string = getRandomWord();
+
+    socket.on("user_match", () => {
+      const filtered = Array.from(getPublicRooms()).filter(
+        (room) => room[1].size < 2
+      );
+
+      if (filtered.length > 0) {
+        // 빈 방 있으면 참여
+        roomName = filtered[getRandomIndex(filtered.length)][0];
+
+        socket.join(roomName);
+        io.to(roomName).emit("send_welcome", roomName, getRandomTopic());
+        socket.emit("send_myword", myWord);
+        socket.to(roomName).emit("send_otherword", otherWord);
+      } else {
+        // 빈 방 없으면 방 생성
+        roomName = shortid.generate();
+
+        socket.join(roomName);
+      }
+    });
+
+    socket.on("start_timer", () => {
+      timeInterval = setInterval(() => {
+        if (time > 0) {
+          time--;
+
+          socket.emit("time_change", time);
+        } else {
+          clearInterval(timeInterval);
+        }
+      }, 1000);
+    });
+
+    socket.on("adjust_time", (amount: number, done: () => void) => {
+      if (time === 120) return;
+
+      if (amount > 0 ? time <= 100 : time >= 20) {
+        time += amount;
+
+        io.to(roomName).emit("adjust_time", time);
+        io.to(roomName).emit(
+          "send_notice",
+          socket.id,
+          amount > 0 ? "시간을 연장했습니다." : "시간을 단축했습니다."
+        );
+        done();
+      }
+    });
+
+    socket.on("sync_time", (currentTime) => {
+      time = currentTime;
+    });
+
+    socket.on("send_message", (msg: string, done: () => void) => {
+      if (time === 120) return;
+
+      socket.to(roomName).emit("send_message", msg);
+      done();
+    });
+
+    socket.on("disconnect", () => {
+      console.log("소켓 연결 해제됨");
+    });
+  });
+}
+
+export default setWebSocket;
