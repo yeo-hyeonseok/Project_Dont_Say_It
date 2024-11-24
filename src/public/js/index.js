8e3f07ca9f10cf36f6d92a6cbbd32a769ac928ce
dugustlr@naver.com <dugustlr@naver.com>
2024-11-24 23:41:32 +0900

    [ADD] 메인 페이지 다크모드 버튼 추가
    - 메인 페이지에 다크 모드와 라이트 모드 제어를 할 수 있는 토글 버튼을 추가

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -9,3 +9,13 @@ function printToastMsg(msg) {
     toastMsg.classList.remove("active");
   }, 2000);
 }
+
+/* 다크 모드 버튼 */
+const darkModeToggle = document.querySelector("input#toggle");
+
+darkModeToggle.addEventListener("change", () => {
+  const darkModeButton = document.querySelector("div.darkmode_button");
+  const icons = darkModeButton.querySelectorAll("i");
+
+  icons.forEach((item) => item.classList.toggle("inactive"));
+});
